d3fd2a8a97abed4815bd5de4410d89d4f82ff117
AbiodunVlad <omonijoabiodun@gmail.com>
2023-09-03 23:22:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// set the initial count
+let count = 0;
+
+// select value and all buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+// console.log(btns);
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else {
+      value.style.color = "blue";
+    }
+    value.textContent = count;
+  });
+});
